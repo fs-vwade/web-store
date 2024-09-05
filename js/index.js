f70141e3504be2c09c84d7f744e47898bd603f18
Victor Wade <p9859817888@gmail.com>
2024-09-05 13:07:01 -1000

    experimenting, +many updates

--- js/index.js
+++ js/index.js
@@ -1,3 +1,54 @@
+/** HTML Component
+ *
+ * @param {Object} collection
+ * collection is a dict {} of items to include
+ * child items should each be a Component type, apiece
+ */
+class Component {
+	constructor(collection:Object=null) {
+		if (collection && collection.name) {
+			this.type = collection.name;
+			this.id = collection.id || null;
+			this.src = collection.src || null;
+			this.href = collection.href || null;
+			this.text = collection.text || null;
+			this.label = collection.class || collection.name;
+			this.children = (
+				Array.isArray(collection.subcomponents)
+			) ? (
+				collection.subcomponents
+			) : (null);
+		} else {
+			console.error(
+				`ERROR: Component object not properly confifgured: ${{
+					is_defined: !! (collection),
+					has_type: !! (collection.name),
+					has_label: !! (collection.class),
+					has_child_elements: (
+						Array.isArray(collection.subcomponents) &&
+						0 < collection.subcomponents.length
+					),
+				}}`
+			);
+		}
+	}
+
+	get element() {
+		const result = document.createElement(this.type);
+		if (this.id) result.id = this.id;
+		if (this.src) result.src = this.src;
+		if (this.href) result.href = this.href;
+		result.innerText = this.text ? this.text : null;
+		result.className = this.label;
+		if (Array.isArray(this.children)){
+			this.children.forEach(e => {
+				item.append(e.element);
+			})
+		}
+		return result;
+	}
+}
+
 /** We create our page elements here
  * Looking at the example page, we need a header, main, and footer section
  *  - The header appears to contain two divs on either side of it
@@ -13,12 +64,131 @@
  *  - Each div flows vertically
  */
 const body = document.body;
-const elements = {
+const elements = [
+	new Component({
+		name: "header",
+		subcomponents: [
+			new Component({
+				name: "nav",
+				class: "nav-header",
+			}),
+			new Component({
+				name: "div",
+				class: "login",
+			}),
+		],
+	}),
+	new Component({
+		name: "main",
+		subcomponents: [
+			new Component({
+				name: "div",
+				class: "search-header",
+				subcomponents: [
+					new Component({name: "h1", class: "site-lettermark", text: "Qwirty"}),
+					new Component({name: "form", class: "search-bar", subcomponents: [
+						new Component({name: "input", class: "input"}),
+						new Component({name: "button", class: "button"}),
+					new Component({name: "a", class: "advert", href: "#"}),
+				]
+			}),
+			new Component({
+				name: "nav",
+				class: "product-categories",
+			}),
+			new Component({
+				name: "section",
+				class: "product-grid",
+			}),
+		],
+	}),
+	new Component({
+		name: "footer",
+		subcomponents: [
+			new Component({
+				name: "nav",
+				class: "site-navigation",
+				meta: "customer-support",
+				subcomponents: [
+					new Component({
+						name: "h2",
+						text: "Navigation Header-2"
+					}),
+					new Component({
+						name: "ul",
+						subcomponents: [
+							"Link 1",
+							"Link 2",
+							"Link 3",
+							"Link 4",
+						].forEach(context => {
+							return new Component({name: "li", text: context})
+						})
+					}),
+				]
+			}),
+			new Component({
+				name: "nav",
+				class: "site-navigation",
+				meta: "sales",
+				subcomponents: [
+					new Component({
+						name: "h2",
+						text: "Navigation Header-2"
+					}),
+					new Component({
+						name: "ul",
+						subcomponents: [
+							"Link 1",
+							"Link 2",
+							"Link 3",
+							"Link 4",
+						].forEach(context => {
+							return new Component({name: "li", text: context})
+						})
+					}),
+				]
+			}),
+			new Component({
+				name: "nav",
+				class: "site-navigation",
+				meta: "about-us",
+				subcomponents: [
+					new Component({
+						name: "h2",
+						text: "Navigation Header-2"
+					}),
+					new Component({
+						name: "ul",
+						subcomponents: [
+							"Link 1",
+							"Link 2",
+							"Link 3",
+							"Link 4",
+						].forEach(context => {
+							return new Component({name: "li", text: context})
+						})
+					}),
+				]
+			}),
+		],
+	}),
+];
+
+
+tmp = {
 	// create major page elements
 	header: {
+		name: "header"
 		// create header sub-elements
-		nav: document.createElement("nav"),
-		login: document.createElement("div"),
+		nav: {
+			// create nav sub-elements
+			name: "header",
+			link
+		},
+		login: {
+			name: "div",
+		},
 	},
 	main: {
 		// create main sub-elements
@@ -34,13 +204,45 @@ const elements = {
 	},
 };
 
-Object.entries(elements).forEach(e => {
-	const section = {
-		name: e[0],
-		items: e[1],
-		element: undefined
-	};
-	const section.element = document.createElement(section.name);
+/** I am considering a second version that will replace the header and footer
+ * with statically placed elements that retain their position at the top and
+ * bottom of the page regardless of the scroll position.
+ *
+ * A fixed location, if you will. */
 
-	body.append(section.element);
-});
+/**
+ *
+ * @param {Object} container_item
+ * @param {HTMLElement} parent_element
+ */
+function create_elements(container_item, parent_element) {
+
+	Object.entries(elements).forEach(e => {
+		const section = {
+			name: e[0],
+			items: e[1],
+		}
+
+		// create parent/base element
+		const section.element = document.createElement(section.name);
+		const section.element.className = section.name;
+
+		// assign children elements to parent/base
+		Object.entries(section.items).forEach(e => {
+			if ((e[0] === "label")) {
+			} else {
+				create_elements(e[1], )
+			}
+		})
+		//if ((e[0] === "label" && typeof(e[1]) === "string")) {
+		//	const section = {
+		//		element: document.createElement(e[1])}
+		//} else {
+		//}
+
+	});
+
+	parent_element.append(section.element);
+}
+
+
